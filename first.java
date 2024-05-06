555f0698b89e60dfc62b42cb39376fb6f124a3a3
root <root@ip-172-31-22-96.ec2.internal>
2024-05-06 07:13:51 +0000

    four

--- first.java
+++ first.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("today topic is github actions,hii"); 
+        System.out.println("today topic is github actions,hi"); 
     }
 }
